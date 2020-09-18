b0f8cf53f9b43df981fbed9518803a8678481516
justinmiller33 <justinryanmiller@comcast.net>
2020-09-18 13:58:09 -0400

    Creating JSON-based handling functions

--- Production/modeling.py
+++ Production/modeling.py
@@ -10,43 +10,14 @@ import random
 import math
 import warnings
 import time
-import pickle
 import re
 import json
 
-"""
-Loading Data Structures from scraping scripts
-"""
-
-# Loading df structure through np
-# Path
-path = "/home/justinmiller/devel/SAPS-public/Data/"
-
-# Function to open master json file
-# Also processes certain data into pandas dataframes and series 
-def loadJson(fileName):
-
-    # Load data as pandas df
-    data = pd.read_json(fileName)
-
-    # Convert back to original dict
-    data = data.to_dict()
-
-    # Loop through each subreddit
-    for subreddit in data.keys():
-
-        # Convert post data into labeled dataframe
-        data[subreddit]['raw']['postData'] = pd.DataFrame(data[subreddit]['raw']['postData'], columns = ["ticker","title","text","flair","unix"])
-
-    return data
-
-data = loadJson(path + "saps.json")
-    
 
 """
 DISCLAIMER:
 This class is being modified from a preexisting model for a different data format
-Most methods will not run on the public json data
+Many methods will not run on the public json data
 
 OVERVIEW:
 Creating a class to generalize a pipeline for different models
@@ -58,11 +29,31 @@ Includes:
 3. ML Models
 4. Ensembling
 
+Notes:
+- Time descriptions |t| are estimates and may vary based on the data
+- CALLS details custom methods called in the function
+- CALLS ignores __init__ and second-level functions
+- REQS:  details methods that must be called before the function can be run
+- REQS: ignores __init__
 
 DIRECTORY:
+
 0.0 __init__
-(data,df,fData) : |insignificant|
+(data,df,fData) : |4 Seconds / 100MB|
 initializing data through the post df, financial data fData, and post/fData keys with data.keys
+CALLS: loadJson
+REQS: n/a
+
+0.1 loadJson
+(pathToJson) : |4 Seconds / 100 MB|
+loading json file from local path and reserializing some pd series and dataframes
+CALLS: n/a
+REQS: n/a
+
+0.2 initProfitDicts
+() : |insignificant|
+initializing (or resetting) profit and missed dicts to handle profit assignments
+CALLS: n/a
 REQS: n/a
 
 
@@ -127,15 +118,212 @@ External REQS: Functions that need to be manually called before function use
 class pipeline:
 
     """
-    0.0-1
-    Section for Initializing Data
+    0.0-2
+    Section for Initializing Class and Loading Data
     """
     
     # Initializing the pipeline to allow for analysis on subsets of the scraped data
-    def __init__(self,data):
-        self.data = data
+    def __init__(self,pathToJson,**kwargs):
+        self.data = pipeline.loadJson(pathToJson)
+        self.subList = list(self.data.keys())
+        self.subTemp = None
+        self.finType = 'inter'
+        self.profitDict = {}
+        self.missedDict = {}
+        
+    # Default function to open master json file
+    # Also processes certain data into pandas dataframes and series
+    def loadJson(pathToJson):
 
-    
+        # Load data as pandas df
+        data = pd.read_json(pathToJson)
+
+        # Convert back to original dict
+        data = data.to_dict()
+
+        # Loop through each subreddit
+        for subreddit in data.keys():
+
+            # Convert post data into labeled dataframe
+            data[subreddit]['raw']['postData'] = pd.DataFrame(data[subreddit]['raw']['postData'], columns = ["ticker","title","text","flair","unix"])
+
+            # For each financial data type
+            for finType in ["inter","intra"]:
+
+                # Get the data and transform it into a pandas time series
+                for key in list(data[subreddit]['raw'][finType].keys()):
+
+                    newData = np.array(data[subreddit]['raw'][finType][key])
+                    newData = pd.Series(data = newData[:,1], index = newData[:,0])
+                    data[subreddit]['raw'][finType][key] = newData
+                    
+        return data
+
+    # Initialize (or reset) profit dictionaries to assign profits for analysis
+    def initProfitDicts(self):
+        for sub in self.subList:
+            self.profitDict[sub] = {}
+            self.missedDict[sub] = []
+
+    """
+    1.0-1.1
+    Minor Functions for Checks and Assertions
+    """
+    def assertInter(self):
+        if self.finType != "inter":
+            raise Exception("This Function can only be called with .finType = 'inter'")
+
+    def assertIntra(self):
+        if self.finType != "intra":
+            raise Exception("This Function can only be called with .finType = 'intra'")
+
+    """
+    2.0-2
+    Assigning Profits and Reorganizing within postData
+    """
+
+    # Assigning profits via hold time
+    def holdForTime(self,days):
+
+        # Assert that self.finType is inter
+        self.assertInter()
+
+        # For each subreddit
+        for sub in self.subList:
+
+            # For each key in the data
+            for key in self.data[sub]['md'][self.finType]['keys']:
+
+                # Get the profit time series for that post
+                dataTemp = self.data[sub]['raw'][self.finType][key]
+
+                # Try-except for recent posts that don't have that many days
+                try:
+                    # Get unique timestamps (only 1 per day)
+                    dayListUnix = np.unique(dataTemp.index)
+                    # Find the target stamp
+                    targetUnix = dayListUnix[days]
+
+                    # Profit is the last index for that day, assuming 4:00 sell
+                    profit = 1 + dataTemp[targetUnix].iloc[-1]
+                    self.profitDict[sub][key] = profit
+
+                except:
+                    # If there isn't enough data for a n-day hold, mark it as missed
+                    self.missedDict[sub].append(key)
+
+    # Function to cut postData to dataframe with profits from time
+    # NOTE... can only run once per test without reloading json
+    def cutMissedProfits(self):
+
+        # Assert inter
+        self.assertInter()
+        
+        # For each subreddit
+        for sub in self.subList:
+
+            # Convert indices to str to match keys to raw postData df
+            saps.data[sub]['raw']['postData'].index = saps.data[sub]['raw']['postData'].index.astype(str)
+
+            # Make new df keeping only posts with profits
+            saps.data[sub]['raw']['postData'] = saps.data[sub]['raw']['postData'].loc[list(saps.profitDict[sub].keys())]
+
+
+    # Adding profits to dataframe for easier manipulation
+    def appendProfitsToDf(self):
+
+        # Assert inter
+        self.assertInter()
+
+        # For each subreddit
+        for sub in self.subList:
+
+            # Create a new column with profit data
+            saps.data[sub]['raw']['postData']['profit'] = list(saps.profitDict[sub].values())
+
+    """
+    3.0-1
+    Supplementing postData and Removing Confounding Vars for ML algos
+    """
+
+    # Function to remove tickers from text to reduce confounding in naive bayes
+    # Not possible to remove everything, will experience time-relevancy bias
+    def removeTickersFromText(self):
+
+        # For each subreddit
+        for sub in self.subList:
+
+            # Assign temporary dataframe
+            dfTemp = saps.data[sub]['raw']['postData']
+
+            # For each row in the df
+            for rowNum in range(len(dfTemp)):
+
+                # Removve the ticker from the text and/or title
+                dfTemp['text'][rowNum] = dfTemp['text'][rowNum].replace(dfTemp.ticker[rowNum],"")
+                dfTemp['title'][rowNum] = dfTemp['title'][rowNum].replace(dfTemp.ticker[rowNum],"")
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     """
     1.0-1
     Section for Functions to parse which profits to test on
@@ -580,9 +768,24 @@ class pipeline:
         
 
 
-    
-# Get pipeline with all data
-saps = pipeline(df,fData)
+# Input local data path
+pathToJson = "/home/justinmiller/devel/SAPS-public/Data/saps.json"
+
+# Initialize pipeline with json formatted data
+saps = pipeline(pathToJson)
+
+# Initialize profit dicts and metadata
+saps.initProfitDicts()
+
+# Get profits for a certain length hold
+saps.holdForTime(10)
+
+# Cut missed rows from dataframe and append profits
+saps.cutMissedProfits()
+saps.appendProfitsToDf()
+
+# Remove tickers from text
+saps.removeTickersFromText()
 
 # Assign main minute value for testing
 # minutes = 60
